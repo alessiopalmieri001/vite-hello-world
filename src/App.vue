ddb54c080872fe6f0df9b1d6504f0f1deeb550b0
alessiopalmieri001 <alessiopalmieri001@gmail.com>
2024-01-18 00:26:00 +0100

    finito esercizio

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script >
+    import headerTitle from './components/headerTitle.vue'
+    export default {
+        data() {
+            return  
+                
+            
+        },
+        components:{
+            headerTitle
+        },
+        
+    }
+</script>
+
+<template>
+    <headerTitle />
+</template>
+
+<style scoped>
+    
+</style>
---
+++ src/components/headerTitle.vue
@@ -0,0 +1,30 @@
+<script >
+    
+    export default {
+        data() {
+            return { 
+                title: 'La mia prima app con Vite!'
+            };
+        },
+        
+        
+    };
+</script>
+
+
+<template>
+    
+        <h1>
+            {{ title }}
+        </h1>
+    
+</template>
+
+<style scoped>
+    h1{
+    text-align: center;
+    font-weight: bold;
+    background-color: blue;
+    padding: 20px;
+}
+</style>
